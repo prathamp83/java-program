d9f70235c680f279eebef711f1cc54a2f917debc
prathamp83 <prathameshpawar302006@gmail.com>
2025-09-03 10:48:30 +0530

    Create variable constant and data type.java

---
+++ variable constant and data type.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static final double PI = 3.14159;
+
+    public static void main(String[] args) {
+        int age = 25;                  
+        double height = 5.9;            
+        char grade = 'A';             
+        boolean isStudent = true;      
+        String name = "Alice";        
+
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is student? " + isStudent);
+        System.out.println("Constant PI: " + PI);
+    }
+}
